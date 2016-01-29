0ca038ad0f4a61b4e1f7a5491de883cbd6558998
andreymyster <and001@gmail.com>
2016-01-29 13:51:22 +0300

    Add client entity

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinarta/activerecord'
+
+# инициализация БД
+set :database, "sqlite3:barbershop.db"
+
+# создание сущности Клиент
+class Client < ActiveRecord::Base
+end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
