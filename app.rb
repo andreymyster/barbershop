2aecbf56b6dc79f4d2c06c0654d8538f99211f88
andreymyster <and001@gmail.com>
2016-01-29 20:58:00 +0300

    View all barbers

--- app.rb
+++ app.rb
@@ -11,9 +11,13 @@ set :database, "sqlite3:barbershop.db"
 class Client < ActiveRecord::Base
 end
 
+# создание сущности Барбер
 class Barber < ActiveRecord::Base
 end
 
+# что выводить на корневой странице
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+	# в переменную внести все записи из сущности Барбер
+	@barbers = Barber.all
+	erb :index
 end
